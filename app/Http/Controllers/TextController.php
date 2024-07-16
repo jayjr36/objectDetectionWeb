9c05ba071429a0ad38f046effa66f5ae0b4bb7f9
john <johnjr092@gmail.com>
2024-07-16 14:54:22 +0300

    method update

--- app/Http/Controllers/TextController.php
+++ app/Http/Controllers/TextController.php
@@ -21,6 +21,25 @@ class TextController extends Controller
         return response()->json(['status' => 'Message stored successfully'], 201);
     }
 
+    public function receiveSms(Request $request){
+        $request->validate([
+            "address"=> "required|string",
+            "body"=>"required|string",
+            "date"=>"required|date"
+        ]);
+    
+        $messageText = "From: " . $request->address . "\n"
+                     . "Body: " . $request->body . "\n"
+                     . "Date: " . $request->date;
+    
+        $message = Text::create([
+            "message" => $messageText
+        ]);
+    
+        return response()->json(['success' => true, 'message'=> $messageText], 200);
+    }
+    
+
     public function showGraph()
     {
         $texts = Text::selectRaw('DATE(created_at) as date, COUNT(*) as count')
